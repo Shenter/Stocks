2e371fccfb90abdd382c7ebf12f609265bbcf534
shenter <shenter@yandex.ru>
2021-06-21 17:06:43 +0300

    Ver 3

--- app/Http/Controllers/MarketController.php
+++ app/Http/Controllers/MarketController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Models\Stock;
+use App\Models\StockHistory;
 
 class MarketController extends Controller
 {
@@ -11,6 +13,17 @@ class MarketController extends Controller
     public function show()
     {
         $stocks = Stock::all();
+        $stocksChangebyDay=[];
+        foreach ($stocks as $stock) {
+            $stocksChangebyDay[$stock->id]=[]  ;
+            }
+        foreach ($stocksChangebyDay as $key=>$value)
+        {
+            $stocksChangebyDay[$key]['change'] =
+                StockHistory::whereDate('created_at',Carbon::today())->oldest()->first();
+        }
+        dd($stocksChangebyDay);
+
         return view('market',['stocks'=>$stocks]);
     }
 }
--- resources/views/market.blade.php
+++ resources/views/market.blade.php
@@ -16,14 +16,11 @@
                             <td>Наличие дивидендов</td>
                             <td>Изменение за день</td>
                             <td>Действия</td>
-
                         </tr>
                         </thead>
                         <tbody>
 
-{{--{{ dd($catalog)}}--}}
                    @foreach($stocks as $stock)
-
                        <tr>
                            <td><a href="{{route('stock.show',['stock'=>$stock['id']])}}">{{$stock['name']}}</a></td>
                            <td>-</td>
@@ -43,11 +40,13 @@
                            </td>
                            <td>
                                    @if ( !Auth::user()->howManystockscanSell($stock['id'])  )
-                                       <button class="bg-blue-500 text-white font-bold py-2 px-4 rounded opacity-50 cursor-not-allowed">Продать
+                                       <button class="bg-blue-500 text-white font-bold py-2 px-4 rounded opacity-50 cursor-not-allowed">
+                                           Продать
                                        </button>
                                    @else
                                    <a href="{{route('stock.sell', ['stock'=>$stock['id']])}}">
-                                           <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">Продать
+                                           <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+                                               Продать
                                            </button>
                                    </a>
                                     @endif
